2dc5a50c627221ab09fc536e11258104abea017f
Nikun Kumar Hotta <nikunh@users.noreply.github.com>
2025-09-23 18:05:07 +0100

    Test DISPATCH_TOKEN with Contents permission

--- src/002-user-setup/install.sh
+++ src/002-user-setup/install.sh
@@ -72,4 +72,4 @@ echo "$USERNAME:$USERNAME" | chpasswd
 echo "Password set to '$USERNAME' for user $USERNAME"
 
 echo "User $USERNAME setup complete."
-# Test DISPATCH_TOKEN for repository dispatch
+# Test DISPATCH_TOKEN with Contents permission
